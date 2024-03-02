b7789a56928c310bb4f2fb0c055992482c6c123c
Rishabh Yadav <github.for.rishabh@gmail.com>
2024-03-02 22:18:50 +0530

    fetched data from snowflake

---
+++ snowflake_connection/First.py
@@ -0,0 +1,41 @@
+import snowflake.connector
+import sys
+sys.path.append('../FileHandling')
+import CSV_Wrapper
+
+# Snowflake account information
+account = 'kha80474'
+username = 'Rishabh'
+password = 'Snowflake@6308'
+warehouse = 'your_warehouse'
+database = 'INTERNAL_TRAINING'
+schema = 'S3_LOAD_TEXAS_ORDER'
+
+# Establish a connection to Snowflake
+conn = snowflake.connector.connect(
+    user=username,
+    password=password,
+    account=account,
+    # warehouse=warehouse,
+    database=database,
+    schema=schema
+)
+
+# Create a cursor object to interact with Snowflake
+cursor = conn.cursor()
+
+# Example: Execute a SQL query
+cursor.execute("SELECT * FROM RISHABH_ASSIGNMENT LIMIT 100")
+
+# Fetch and print the results
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+csvHandler = CSV_Wrapper.CSVHandler('data.csv')
+
+csvHandler.overwrite_csv(rows)
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
